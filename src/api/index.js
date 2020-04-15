57397f47c2ff55335e6a0823db33026f05a54f29
Valeryia Chyzhova <chyzhval@fel.cvut.cz>
2020-04-15 13:56:50 +0200

    modal window for trip details

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,18 @@
-const api_url = "http://localhost:8080";
+const api_url = "http://tash.wtf/api";
 
 export async function getTripsApi(by, order) {
   try {
-    const response = await fetch(`${api_url}/trips/sort?by=${by}&order=${order}`);
+    const response = await fetch(`${api_url}/trips?sortBy=${by}&order=${order}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function getTripDetailApi(id) {
+  try {
+    const response = await fetch(`${api_url}/trips/${id}`);
     const data = await response.json();
     return data;
   } catch (error) {
